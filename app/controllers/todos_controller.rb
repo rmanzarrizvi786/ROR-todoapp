a1a74798f99918195c6cf4268aa302dfbae9a1b2
manzar <manzarrizvi92@gmail.com>
2018-12-24 17:59:59 +0500

    add edit action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,6 +17,21 @@ class TodosController < ApplicationController
 		@todo = Todo.find(params[:id])
 	end
 
+	def edit
+		@todo = Todo.find(params[:id])
+	end
+
+	def update
+		@todo = Todo.find(params[:id])
+		if @todo.update(todo_params)
+			flash[:notie] = "Todo was successfully updated"
+			redirect_to todo_path(@todo)
+		else
+			render 'edit'
+		end
+
+	end
+
 	private
 
 	# whitelisitng what we allow
